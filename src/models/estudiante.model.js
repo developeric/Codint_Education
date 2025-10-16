97ba5531c29337c4e8102a943e3c13043bb7327d
develop <patinetakiller564@gmail.com>
2025-10-16 12:15:50 -0300

    Prueba de Gemini

---
+++ src/models/estudiante.model.js
@@ -0,0 +1,19 @@
+import { Schema, Types, model } from "mongoose";
+import { type } from "os";
+
+const estudianteSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
